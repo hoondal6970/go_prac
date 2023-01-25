ae4bfa7b6a4a1e4f88b2cc5236c9e1cd565d7b04
hoondal69 <davidtwins6970@gmail.com>
2023-01-25 09:49:22 +0900

    #3 url checker beginning

--- learngo/main.go
+++ learngo/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/hoondal6970/learngo/learngo/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "Greetings"
-	dictionary.Add(word, definition)
-	dictionary.Update(word, "modified")
-	dictionary.Delete(word)
-	found, err := dictionary.Search(word)
-	fmt.Println(found, err)
 
 }
