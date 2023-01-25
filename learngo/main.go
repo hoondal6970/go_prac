9d8692e72cd372b6db28e2021bee2a7727504a51
hoondal69 <davidtwins6970@gmail.com>
2023-01-25 18:19:59 +0900

    scrapping done. make scrapper package

--- learngo/main.go
+++ learngo/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/hoondal6970/learngo/learngo/scrapper"
+	"github.com/labstack/echo"
+)
+
+func handleHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func main() {
+	scrapper.Scrape("term")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.Logger.Fatal(e.Start(":1234"))
 }
